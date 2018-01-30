b26bb1fe38805bd96423538d3b6d938df049cd94
John Vorwald <john.vorwald@dice.com>
2018-01-30 15:36:35 -0600

    lessening the query.

--- app.js
+++ app.js
@@ -41,13 +41,7 @@ app.get('/list', function (req, res) {
 app.get('/proxylist', function (req, res) {
     const params = {
         TableName: 'pocEmailProxy',
-        KeyConditionExpression: '#proxy = :proxy',
-        ExpressionAttributeNames:{
-            "#proxy": "proxy"
-        },
-        ExpressionAttributeValues: {
-            ":proxy":proxy
-        }
+        KeyConditionExpression: '#proxy = :proxy'
     }
     proxydb.query(params, (err, data) => {
         if (err) {
